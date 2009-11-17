2bb96fcfb2fdc2fa6ddec47e11f61b0ed16d2794
emmank <herman_syam@yahoo.com>
2009-11-17 12:19:05 +0800

    update patient hari kemarin

--- includes/main.class.php
+++ includes/main.class.php
@@ -31,6 +31,15 @@ class mainModule
         'perempuan',
         'laki-laki'
     );
+
+    var $goldarah = array(
+        'A',
+        'B',
+        'AB',
+        'O'
+    );
+
+
     var $agama = array(
         'islam',
         'kristen protestan',
@@ -121,6 +130,17 @@ class mainModule
         }
         return $result;
     }
+    // ------ add by emmank ----
+    function __get_goldarah_options(){
+        $result = array();
+        krsort($this->goldarah);
+        foreach($this->goldarah as $key => $value){
+            $result[$key] = ucwords(__t($value));
+        }
+        return $result;
+    }
+
+
 
     function __get_bahasa_options(){
         $result = array();
